2ccfe4aef9d0caab989f5cc146a37c35dd99cfa1
Taylor <Taylor@Zipcoder04s-MacBook-Pro.local>
2016-01-25 12:15:11 -0500

    First commit, all current files

---
+++ src/main/java/Initialization1Pt2.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Taylor on 1/20/16.
+ */
+
+class BoringThing {
+}
+
+class ColorfulThing {
+
+    public enum Color{
+        RED, BLUE, YELLOW
+    }
+    String color;
+
+    public ColorfulThing(Color theColor) { // pass in an enum Color object called theColor.
+        color = String.valueOf(theColor); //store what is passed in as color. Make that a global variable.
+//get the string value of the enum ^reference that is passed in, so again, color = theColor
+    }
+
+    public String getColor() {
+
+        return color;
+    }
+
+
+}
+
+public class Initialization1Pt2 {
+    public static void main(String[] args) {
+        ColorfulThing colorThing = new ColorfulThing(ColorfulThing.Color.RED); // call object type.enumname.enumvalue
+        System.out.println(colorThing.getColor()); //call getColor method on colorThing object
+    }
+}
